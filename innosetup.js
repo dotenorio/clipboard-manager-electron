14815489aca15f37b554a221513ba6a487d9bee1
Fernando Migliorini Tenório <dotenorio@gmail.com>
2020-03-02 17:18:04 -0300

    feat: New build process (WIP)

--- innosetup.js
+++
@@ -1,7 +0,0 @@
-require('innosetup-compiler')('./clipboard-manager-electron-win32-ia32.iss', {
-  gui: false,
-  verbose: true
-}, function (error) {
-  if (error) throw new Error(error)
-  console.log('The innosetup compiled with success!')
-})
