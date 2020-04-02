0b627ff0020727af60e66b4a3085f852724cd4d2
Fernando Migliorini Tenório <dotenorio@gmail.com>
2020-04-02 16:02:03 -0300

    ci: 🎡 Fix code for linux an other things for ci

--- screen_lock_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
-  while read x; do
-    case "$x" in 
-      *"boolean true"*) echo SCREEN_LOCKED;;
-      *"boolean false"*) echo SCREEN_UNLOCKED;;  
-    esac
-  done
